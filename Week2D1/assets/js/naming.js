fdf8a1d0cfbfa5a85ce2bc312f4a2979f2e72733
Umberto Barera <43371465+UmbeBDH@users.noreply.github.com>
2023-02-06 17:09:43 +0100

    Add files via upload

---
+++ Week2D1/assets/js/naming.js
@@ -0,0 +1,14 @@
+document.getElementsByTagName('un saluto')
+var petPreferito = 'cricetO';
+console.log(petPreferito);
+
+var pet_ = '_leonE';
+console.log(pet_);
+
+var pet = 'giraffa_';
+console.log(pet);
+
+
+document.write('<h1>Iniziamo la nostra pratica</h1>');
+document.write('<p>output scritto da javascript</p>');
+document.write('<p>pantera scritto da javascript</p>');
